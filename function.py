97ef14d13c22b490114b3c009522ad1d16b9271a
aditya <adyaabhogale6@gmail.com>
2026-05-20 13:06:51 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    return a + b
+
+print(add(2, 3))
+print(add(10, 5))
+print(add(7, 8))
---
+++ input.py
@@ -0,0 +1,12 @@
+
+name = input("Enter your name: ")
+
+print("Hello,", name)
+
+age = input("Enter your age: ")
+
+age = int(age)
+
+print("Your age is:", age)
+next_year_age = age + 1
+print("Next year, you will be:", next_year_age)
