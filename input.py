902b0fc7267c2b13b59d43c9add3f66025229abb
aditya <adyaabhogale6@gmail.com>
2026-05-21 18:58:45 +0530

    Refactor input.py to remove duplicate code

--- input.py
+++ input.py
@@ -1,12 +1,7 @@
-
 name = input("Enter your name: ")
-
 print("Hello,", name)
-
 age = input("Enter your age: ")
-
 age = int(age)
-
 print("Your age is:", age)
 next_year_age = age + 1
 print("Next year, you will be:", next_year_age)
