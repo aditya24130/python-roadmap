db1ab3c3a9148d1a790b7243f54564742a7932e2
aditya <adyaabhogale6@gmail.com>
2026-05-21 18:37:58 +0530

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,29 +1,21 @@
 class Animal:
 
-    def __init__(self, name):
+        def __init__(self, name):
         self.name = name
 
-    def sound(self):
-        print("Animal makes a sound")
+       def sound(self):
+       print("Animal makes a sound")
+       class Dog(Animal):
 
+      def __init__(self, name, breed):
+      super().__init__(name)
 
-class Dog(Animal):
+       self.breed = breed
+       def sound(self):
+       print("Dog barks")
+       d1 = Dog("Tommy", "Labrador")
 
-    def __init__(self, name, breed):
+         print("Name:", d1.name)
+         print("Breed:", d1.breed)
 
-        
-        super().__init__(name)
-
-        self.breed = breed
-
-
-    def sound(self):
-        print("Dog barks")
-
-
-d1 = Dog("Tommy", "Labrador")
-
-print("Name:", d1.name)
-print("Breed:", d1.breed)
-
-d1.sound()
+         d1.sound()
