0044157533be9d4c77899b2f69c2251110b98734
aditya <adyaabhogale6@gmail.com>
2026-05-21 15:04:31 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,17 @@
+
+with open("input.txt", "r") as file:
+    content = file.read()
+
+print("Using read():")
+print(content)
+
+with open("input.txt", "r") as file:
+    lines = file.readlines()
+
+print("\nUsing readlines():")
+print(lines)
+
+with open("output.txt", "w") as new_file:
+    new_file.write(content)
+
+print("\Content copied to output.txt")
