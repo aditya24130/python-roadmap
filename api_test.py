cf7968e87b39fa117cc1e2ebd0cd787b852c7846
aditya bhogale <adyaabhogale6@gmail.com>
2026-05-29 15:24:16 +0530

    Add username print statement to api_test.py
    Added print statement for username in API test.

--- api_test.py
+++ api_test.py
@@ -1,11 +1,8 @@
 import requests
 
 url = "https://jsonplaceholder.typicode.com/users/1"
-
 response = requests.get(url)
-
 data = response.json()
-
 print("नाव :", data["name"])
 print("ईमेल :", data["email"])
 print("शहर :", data["address"]["city"])
